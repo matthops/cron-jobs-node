b29139bc82a5381a6c887178724986fbb32221b1
Matt Hopkins <matthops@gmail.com>
2019-10-30 23:03:17 -0700

    a buncha work on the throttler

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const send = require('./send');
 const massive = require('massive');
 const bodyParser = require('body-parser');
 const nexmoController = require('./send.js');
+const SMSThrottler = require('./throttler');
+const Nexmo = require('nexmo');
+const plaid = require('plaid');
 require('dotenv').config();
 
-const { CONNECTION_STRING } = process.env;
+const { CONNECTION_STRING, NEXMO_API_KEY, NEXMO_API_SECRET } = process.env;
 
-const plaid = require('plaid');
+const nexmo = new Nexmo({
+  apiKey: NEXMO_API_KEY,
+  apiSecret: NEXMO_API_SECRET
+});
 
 const {
   PLAID_CLIENT_ID,
   PLAID_SECRET,
-  REACT_APP_PLAID_PUBLIC_KEY
+  REACT_APP_PLAID_PUBLIC_KEY,
+  NUMBER_TO,
+  NUMBER_FROM
 } = process.env;
 
 const client = new plaid.Client(
@@ -23,22 +31,39 @@ const client = new plaid.Client(
   plaid.environments.development
 );
 
+const throttler = new SMSThrottler({
+  nexmoInstance: nexmo,
+  accountOptions: { minTime: 2000 }
+});
+
+const from = NUMBER_FROM;
+const to = NUMBER_TO;
+
+function completed(err, result) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('completed', result);
+  }
+}
+
+const getSum = (total, num) => {
+  return total + num;
+};
+
 const app = express();
 
 app.use(bodyParser.json());
+let categoryArr = [];
 
 massive(CONNECTION_STRING).then(db => {
   app.set('db', db);
   console.log('db connected');
+  let objArr = [];
 
   db.get_rules().then(results => {
-    let categoryArr = [];
-    console.log('HIT db.get_rules');
-
-    results.forEach(e => {
-      console.log(e.user_id, e.category, e.frequency);
-
-      db.get_item(e.user_id).then(response => {
+    results.forEach(rule => {
+      db.get_item(rule.user_id).then(response => {
         client.getTransactions(
           response[0].access_token,
           '2019-10-12',
@@ -48,40 +73,31 @@ massive(CONNECTION_STRING).then(db => {
             offset: 0
           },
           (err, response) => {
-            // Handle err
             if (err) {
               console.log('error', err);
             }
             let transactions = response.transactions;
-            console.log('category', e.category);
-            const getSum = (total, num) => {
-              return total + num;
-            };
-            // const amountsArr = [];
-            // response.forEach(x => amountsArr.push(x.amount));
-            // const amountSpent = amountsArr.reduce(getSum, 0);
+            let countArr = [];
 
-            const getCount = () => {
-              let countArr = [];
-              console.log('inside get count');
-              transactions.forEach(y => {
-                y.category.forEach(z =>
-                  z.includes(e.category) ? countArr.push(y.amount) : null
-                );
-              });
-              console.log('COUNTCOUNT', countArr);
+            transactions.forEach(transaction => {
+              transaction.category.forEach(catArr =>
+                catArr.includes(rule.category)
+                  ? countArr.push(transaction.amount)
+                  : null
+              );
+            });
 
-              const amountSum = countArr.reduce(getSum, 0);
-              return amountSum;
-              // };
-            };
-            const amount = getCount();
-            console.log(`This week, you spent $${amount} on ${e.category}`);
-
-            nexmoController.nexmoJob(
-              `This week, you spent $${amount} on ${e.category}`
-            );
-            // );
+            const text = `This week, you spent $${countArr
+              .reduce(getSum, 0)
+              .toLocaleString()} on ${rule.category}`;
+            console.log('texttexttext', text);
+            objArr.push({ from, to, text, callback: completed });
+            // console.log('OBJARROBJARROBJARR', objArr);
+            if (objArr.length === results.length) {
+              for (let i = 0; i < objArr.length; ++i) {
+                throttler.queue(objArr[i]);
+              }
+            }
           }
         );
       });
